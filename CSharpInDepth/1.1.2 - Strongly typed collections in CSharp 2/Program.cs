8ce3e7d38c6779d9a5bb0b5505180d42ea6200ad
Axel Prieto <prietoaxel@gmail.com>
2014-12-19 21:30:38 -0300

    Add skeleton

---
+++ CSharpInDepth/1.1.2 - Strongly typed collections in CSharp 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInDepth
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
