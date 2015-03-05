6c7e1299134f528452028f37898dd9bfec450450
Axel Prieto <prietoaxel@gmail.com>
2015-03-04 22:00:09 -0300

    minor changes

--- CSharpInDepth/1.1.2 - Strongly typed collections in CSharp 2/Program.cs
+++ CSharpInDepth/1.1.2 - Strongly typed collections in CSharp 2/Program.cs
@@ -2,16 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CSharpInDepth
 {
     public class Program
     {
+        static Dictionary<string, int> CountWords(string text)
+        {
+            var frequencies = new Dictionary<string, int>();
+
+            string[] words = Regex.Split(text, @"\W+");
+
+            foreach (string word in words)
+            {
+                if (frequencies.ContainsKey(word))
+                {
+                    frequencies[word]++;
+                }
+                else
+                {
+                    frequencies[word] = 1;
+                }
+            }
+
+            return frequencies;
+        }
+
         static void Main()
         {
-            Console.WriteLine("Hello world!");
-            Console.ReadLine();
+            string text = @"Do you like green eggs and ham?
+                            I do not like them, Sam-I-am,
+                            I do not like green eggs and ham.";
+
+            var frequencies = CountWords(text);
+
+            foreach (KeyValuePair<string, int> entry in frequencies)
+            {
+                string word = entry.Key;
+                int frequency = entry.Value;
+                Console.WriteLine("{0}: {1}", word, frequency);
+            }
         }
     }
 }
